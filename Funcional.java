7897f9ac7eb8083cc883ad0ce7c8a7deadbe76ac
Vinícius Becker de Souza <>
2021-05-17 17:08:00 +0000

    Inclui funcional e imperativo.java

---
+++ Funcional.java
@@ -0,0 +1,9 @@
+import java.util.function.UnaryOperator;
+
+public class Funcional{
+  public static void main(String[] args){
+    UnaryOperator<Integer> calcularValorVezes3 = valor -> valor * 3;
+    int valor = 10;
+    System.out.println("O resultado é ::" + calcularValorVezes3.apply(valor));
+  }
+}
---
+++ Imperativo.java
@@ -0,0 +1,7 @@
+public class Imperativo{
+  public static void main(String[] args){
+    int valor = 10;
+    int reasultado = valor * 3;
+    System.out.println("O resultado é ::" + reasultado);
+  }
+}
